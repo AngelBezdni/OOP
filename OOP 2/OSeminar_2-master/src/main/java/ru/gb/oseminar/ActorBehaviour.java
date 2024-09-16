c07c72a3777fbd97cf75572c11f90caac8c6ae8a
AngelBezdni <90098087+AngelBezdni@users.noreply.github.com>
2024-09-16 17:16:11 +0300

    Delete OOP 2/OSeminar_2-master directory

--- OOP 2/OSeminar_2-master/src/main/java/ru/gb/oseminar/ActorBehaviour.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.oseminar;
-
-public interface ActorBehaviour {
-    void setMakeOrder(boolean makeOrder);
-    void setTakeOrder(boolean pickUpOrder);
-    boolean isMakeOrder();
-    boolean isTakeOrder();
-}
--- OOP 2/OSeminar_2-master/src/main/java/ru/gb/oseminar/QueueBehaviour.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.oseminar;
-
-public interface QueueBehaviour {
-    void takeInQueue(Actor actor);
-    void takeOrders();
-    void giveOrders();
-    void releaseFromQueue();
-}
